57d1e3721c4677c0b5490493631193a271b38278
silviustoian <silviu.stoian96@gmail.com>
2021-03-15 17:08:01 +0200

    new tab links

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
+
+
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
 });
