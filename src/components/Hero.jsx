2a34bcba58c8bbbc524263f291dd69dd77f77c7a
Silviu Stoian <silviustoian@192.168.1.130>
2025-05-08 20:40:04 +0300

    finish

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,28 +1,52 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/vestruuProfile.png"
+import profilePic from "../assets/vestruuProfile.png";
+import { motion } from "motion/react";
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: { x: 0, opacity: 1, transition: { duration: 0.5, delay: delay } },
+});
 const Hero = () => {
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Silviu Stoian
-            </h1>
-            <span
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
               className="bg-gradient-to-r from-[#45E8FA] via-[#FCF7F8] to-[#45E8FA]
              bg-clip-text text-4xl tracking-tight text-transparent"
             >
               FrontEnd Developer
-            </span>
-            <p className="my-2 max-w-xl py-8 font-light tracking-tighter">{HERO_CONTENT}</p>
+            </motion.span>
+            <motion.p
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-8 font-light tracking-tighter"
+            >
+              {HERO_CONTENT}
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
-            <div className="flex justify-center">
-                <img src={profilePic} alt="" />
-            </div>
+          <div className="flex justify-center">
+            <motion.img 
+            initial={{x:100, opacity:0}}
+            animate={{x:0, opacity:1}} 
+            transition= {{duration:0.8, delay:1.2}}
+            src={profilePic} alt="" />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -5,38 +5,95 @@ import { FaCss3Alt } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
 import { FaAngular } from "react-icons/fa";
-// import { RiReactjsLine } from "react-icons/ri";
+import { motion } from "motion/react";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
       <h1 className="my-20 text-center text-4xl">Technologies</h1>
-      <div className="flex flex-wrap justify-center items-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.div
+      whileInView={{opacity:1, x:0}}
+      initial= {{opacity:0, x: -100}}
+      transition={{duration:1.5}}
+      className="flex flex-wrap justify-center items-center gap-4">
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <IoLogoJavascript className="text-7xl text-yellow-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaHtml5 className="text-7xl text-[#e5532f]" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaCss3Alt className="text-7xl text-[#2e53e5]" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiTailwindCssFill className="text-7xl text-[#39bef8]" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaAngular className="text-7xl text-[#d70230]" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaGithub className="text-7xl text-white-400" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaWordpress className="text-7xl text-white-400" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
